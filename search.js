bb709845e6be0d7ccd62fa9d5cea30dc72a16998
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-29 15:22:58 -0500

    changed name

--- search.js
+++ search.js
@@ -36,7 +36,7 @@ function binarySearch(array, value, start, end) {
     }
 };
 
-function bs(tree,values=[]){
+function bfs(tree,values=[]){
     const queue = new Queue();
 
     // add to queue line from tree
@@ -107,4 +107,26 @@ return officers.postOrder()
 
 //in-order 14 15 19 25 27 35 79 89 90 91 
 //pre-order 35 25 15 14 19 27 89 79 91 90. 
-//post-order
+//post-order 
+
+const sharePrices = [128, 97, 121, 123, 98, 97, 105];
+
+const calcBuyDay = function (sharePrices) {
+
+  let highestGain = 0;
+  let buyDay = null;
+
+  for (let d = 1; d < sharePrices.length; d++) {
+
+    const gain = sharePrices[d] - sharePrices[d-1];
+
+    if (gain > highestGain) {
+      highestGain = gain;
+      buyDay = d-1;
+    }
+  }
+
+  return buyDay;
+};
+
+console.log(calcBuyDay(sharePrices));
